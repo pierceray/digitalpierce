7ec6f1245e1906226f9638a845c2ed60a83d1de5
Ray Pierce <ray@digitalpierce.com>
2016-01-31 23:31:21 -0600

    removing old config files.

--- configs/grunt_configs/uglifyConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	main: {
-		expand: true,     // Enable dynamic expansion.
-		cwd: "src/js/",      // Src matches are relative to this path.
-		src: ["**/*.js"], // Actual pattern(s) to match.
-		dest: "build/js/",   // Destination path prefix.
-		ext: ".js"   // Dest filepaths will have this extension.
-	}
-};
