391452d6bef181f61a6317dc15de1d18e4e4198e
Ray Pierce <ray@digitalpierce.com>
2022-08-19 23:27:56 -0500

    feat: update font for sticker display
    - removed Loved By the King font
    - added Homemade Apple and reduced the font size

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,15 +7,8 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <link
-          href="https://fonts.googleapis.com/css?family=Loved+by+the+King&display=swap&text=Ray%20Pierce"
+          href="https://fonts.googleapis.com/css2?family=Homemade+Apple&display=swap&text=Ray%20Pierce"
           rel="stylesheet"
-        />
-        <link
-          rel="preload"
-          as="font"
-          crossOrigin="crossorigin"
-          type="font/woff2"
-          href="https://fonts.gstatic.com/s/lovedbytheking/v9/Gw6gwdP76VDVJNXerebZxUMeRXUF2MiKn3BM2g65.woff2"
         />
       </Head>
       <body>
